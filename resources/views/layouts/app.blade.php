a2cacb66858a2c845ea0b2ac7c54e84f894c1c49
carlosdevletian <carlosdevletian@gmail.com>
2018-01-08 21:55:03 -0500

    Agregado logo para tab en chrome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="@yield('description')">
+    <link rel="icon" type="image/png" sizes="16x16" href="/logo.png">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
