61704b36de587b489f719ddb733899b478e269c6
carlosdevletian <carlosdevletian@gmail.com>
2018-02-09 17:29:29 -0400

    Laravel 5.6 Upgrade

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,18 +12,19 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
-
-    /**
-     * The current proxy header mappings.
-     *
-     * @var array
-     */
-    protected $headers = [
+    protected $proxies = [
         Request::HEADER_FORWARDED => 'FORWARDED',
         Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
         Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
         Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
         Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
     ];
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var string
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
 }
